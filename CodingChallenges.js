681d2607d3bbcf97f86b4fecc37177d6d1760fff
Nolan <ndietz2121@gmail.com>
2023-04-18 22:12:40 -0500

    Coding Challenge 2 complete

--- CodingChallenges.js
+++ CodingChallenges.js
@@ -7,3 +7,24 @@ function triArea(base, height) {
 }
 
 console.log(triArea(6,2));
+
+//Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+function minMax(arr) {
+	let min = Infinity;
+	let max = -Infinity;
+	newArr = []
+	
+	for(let number of arr){
+		if(number > max)
+			max = number
+			
+		if(number < min)
+			min = number
+			}
+		
+		newArr.push(min)
+		newArr.push(max)
+	
+	return newArr;
+}
