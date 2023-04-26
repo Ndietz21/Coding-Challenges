682a33272c2b970c6b39475d37ce68cd9f3b919f
Nolan <ndietz2121@gmail.com>
2023-04-26 17:44:11 -0500

    Challenge 3 complete

--- CodingChallenge2.js
+++ CodingChallenge2.js
@@ -1,13 +1,3 @@
-//Write a function that takes the base and height of a triangle and return its area.
-
-function triArea(base, height) {
-
-    return (base * height) / 2;
-	
-}
-
-console.log(triArea(6,2));
-
 //Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
 
 function minMax(arr) {
