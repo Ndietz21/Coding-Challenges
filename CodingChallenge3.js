f0de27989292b42c1918091320fcdbb98e5aa8c0
Nolan <ndietz2121@gmail.com>
2023-05-06 13:10:01 -0500

    complete coding challenge 4

--- CodingChallenge3.js
+++ CodingChallenge3.js
@@ -1,6 +1,6 @@
 // First Reverse
 // Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. 
-// For example: if the input string is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
+// For example: if the input sgit puistring is "Hello World and Coders" then your program should return the string sredoC dna dlroW olleH.
 
 function FirstReverse(str) { 
 
